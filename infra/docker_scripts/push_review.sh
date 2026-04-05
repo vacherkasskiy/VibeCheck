bafb1a7abf650fff88d0d69bd302fe1a00145900
Vitaliy Cherkasskiy <vacherkasskiy@gmail.com>
2026-04-05 18:20:00 +0300

    use latest image in manifests

---
+++ infra/docker_scripts/push_review.sh
@@ -0,0 +1,12 @@
+docker buildx create --name multi --use || docker buildx use multi
+docker buildx inspect --bootstrap
+
+# CHANGE VERSION BEFORE PUSH
+
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  -t ikeeo/vibecheck-review-service:4.1 \
+  -t ikeeo/vibecheck-review-service:latest \
+  -f ../../backend/ReviewService/ReviewService.Gateway/Dockerfile \
+  ../ \
+  --push

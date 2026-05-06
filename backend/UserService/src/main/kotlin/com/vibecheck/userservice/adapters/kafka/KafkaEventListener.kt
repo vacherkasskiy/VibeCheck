63edf0838c5a8b644f7aaa71419e3ea00e7e4d5d
gonefladvedotov <vvfedotov@edu.hse.ru>
2026-05-05 22:39:45 -0700

    fix

--- backend/UserService/src/main/kotlin/com/vibecheck/userservice/adapters/kafka/KafkaEventListener.kt
+++ backend/UserService/src/main/kotlin/com/vibecheck/userservice/adapters/kafka/KafkaEventListener.kt
@@ -2,10 +2,16 @@ package com.vibecheck.userservice.adapters.kafka
 
 import com.vibecheck.userservice.domain.events.UserInfoCreatedEvent
 import com.vibecheck.userservice.domain.events.UserInfoUpdatedEvent
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
 
 @Service
+@ConditionalOnProperty(
+    prefix = "user-service.kafka",
+    name = ["disabled"],
+    havingValue = "false",
+)
 class KafkaEventListener(
     private val producer: KafkaEventProducer,
     private val userProfileEventMapper: UserProfileEventMapper,
